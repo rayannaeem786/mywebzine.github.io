ab3cda7288cb801681f18edf593fb608450d7380
rayannaeem786 <chaudhryrayyan68@gmail.com>
2020-04-30 11:30:08 +0100

    my webzine
    i am happy

---
+++ js/script.js
@@ -0,0 +1,59 @@
+
+// $(document).ready(function () {
+//     $('a.smoth-scroll').click(function (e) {
+//         e.preventDefault();
+//         var section = $(this).attr('href');
+//         $('html,body').animate({
+//             scrollTop: $(section).offset().top
+//         }, 2000);
+//     });
+// });
+
+// $(document).ready(function(){
+//     $('a.smoth-scroll').click(function(e){
+//         e.preventDefault();
+//         var section = $(this).attr('href');
+//         $('html,body').animate({
+//             scrollTop:$(section).offset().top
+//         },1000);
+//     });
+// });
+// $(document).ready(function(){
+//     $('a.smoth-scroll').click(function(e){
+//         e.preventDefault();
+//         var section = $(this).attr('href');
+//         $('html,body').animate({
+//             scrollTop: $(section).offset().top
+//         },1000);
+//     });
+// });
+
+
+
+// $(document).ready(function(){
+//     $('a.smoth-scroll').click(function(e){
+//         e.preventDefault();
+//         var section = $(this).attr('href');
+//         $('html,body').animate({
+//             scrollTop:$(section).offset().top
+//         },1000);
+//     });
+// });
+// $(document).ready(function(){
+//     $('a.smoth-scroll').click(function(e){
+//         e.preventDefault();
+//         var section = $(this).attr('href');
+//         $('html,body').animate({
+//             scrollTop: $(section).offset().top
+//         },1000);
+//     });
+// });
+$(document).ready(function(){
+    $('a.smoth-scroll').click(function(e){
+        e.preventDefault();
+        var section = $(this).attr('href');
+        $('html,body').animate({
+            scrollTop: $(section).offset().top
+        },1000)
+    })
+})
